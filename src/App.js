9efe6822c777755fbaaa382493e606e87c930898
Leifa129 <leif_xx@hotmail.com>
2020-10-06 11:47:44 +0200

    Playing around with CSS. I want to get CSS back into my fingers. Dont mind the mess :)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-           <Card progress={13} title={'Interview Preparation Kit'} problemsSolved={13} totalProblems={69} description={'INTERVIEW PREPARATION'}/>
+            <Card progress={13} title={'Interview Preparation Kit'} problemsSolved={13} totalProblems={69} description={'INTERVIEW PREPARATION'}/>
             <Card progress={50} title={'Problem Solving Kit'} problemsSolved={42} totalProblems={84} description={'PROBLEM SOLVING'}/>
         </div>
     );
